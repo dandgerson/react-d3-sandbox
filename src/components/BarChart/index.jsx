364bc4f980444818f9d9d75a8e80fd9a33ea6e55
Sergei Komarov <sergeikomarov@MacBook-Pro-Roman.local>
2020-03-03 00:17:35 +0300

    1. refactor function arguments 2. drop getters

--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -1,63 +1,55 @@
-import React, { useState, useEffect, useRef } from 'react'
-import classNames from 'classnames'
+import React, { useState, useEffect, useRef } from "react";
+import classNames from "classnames";
 
-import { fetchCSVData } from 'helpers'
-import { rowConverter } from './helpers'
-import renderChart from './renderChart'
+import { fetchCSVData } from "helpers";
+import { rowConverter } from "./helpers";
+import renderChart from "./renderChart";
 
-import style from './BarChart.module.scss'
+import style from "./BarChart.module.scss";
 
-const BarChart = ({ dataPath, ...props }) => {
-  const [data, setData] = useState([])
+const BarChart = ({ dataPath, ...restProps }) => {
+  const [data, setData] = useState([]);
   const refs = {
-    svg: useRef(null),
-  }
+    svg: useRef(null)
+  };
   const elems = {
     bars: [],
-    labels: [],
-  }
+    labels: []
+  };
 
   useEffect(() => {
-    fetchCSVData(dataPath, rowConverter, setData)
-  }, [dataPath])
+    fetchCSVData(dataPath, rowConverter, setData);
+  }, [dataPath]);
 
   useEffect(() => {
-    data.length && renderChart(data, refs, elems)
-  }, [data, refs, elems])
+    data.length && renderChart({ data, refs, elems });
+  }, [data, refs, elems]);
 
-  // console.log({data})
+  console.log({ data });
 
-  return data.length && (
-    <div
-      className={classNames(
-        style.root,
-      )}
-    >
-      <h1>Bar - chart</h1>
-      <svg
-        ref={refs.svg}
-      >
-        {data.map((d, i) => (
-          <g key={i}>
-            <text
-              ref={(elem) => elem && elems.labels.push(elem)}
-              className={classNames(
-                style.label,
-              )}
-            >
-              {`${d.mount / 1000}k`}
-            </text>
-            <rect
-              ref={(elem) => elem && elems.bars.push(elem)}
-              className={classNames(
-                style.bar,
-              )}
-            />
-          </g>
-        ))}
-      </svg>
-    </div>
-  )
-}
+  return (
+    data.length && (
+      <div className={classNames(style.root)}>
+        <h1>Bar - chart</h1>
+        <svg ref={refs.svg}>
+          {data.map((d, i) => (
+            <g key={i}>
+              <rect
+                ref={elem => elem && elems.bars.push(elem)}
+                className={classNames(style.bar)}
+              />
+              <text
+                ref={elem => elem && elems.labels.push(elem)}
+                className={classNames(style.label)}
+              >
+                {`${d.mount / 1000}k`}
+              </text>
+            </g>
+          ))}
+        </svg>
+      </div>
+    )
+  );
+};
 
-export default BarChart
+export default BarChart;
--- src/components/BarChart/renderChart.js
+++ src/components/BarChart/renderChart.js
@@ -1,43 +1,35 @@
-import * as d3 from 'd3'
+import * as d3 from "d3";
 
-import {
-  simpleCompose,
-} from 'helpers'
+import { simpleCompose } from "helpers";
 
-import {
-  logger,
-  drawRects,
-  drawLabels,
-} from './helpers'
+import { logger, drawRects, drawLabels } from "./helpers";
 
-const renderChart = (data, refs, elems, setData) => simpleCompose(
-  logger,
-  drawRects,
-  drawLabels,
-)({
-  data,
-  refs,
-  elems,
-  svg: d3.select(refs.svg.current),
+const renderChart = ({ data, refs, elems }) =>
+  simpleCompose(
+    logger,
+    drawRects,
+    drawLabels
+  )({
+    data,
+    refs,
+    elems,
+    svg: d3.select(refs.svg.current),
 
-  get width() {
-    return refs.svg.current.getBoundingClientRect().width
-  },
-  get height() {
-    return refs.svg.current.getBoundingClientRect().height
-  },
+    width: refs.svg.current.getBoundingClientRect().width,
+    height: refs.svg.current.getBoundingClientRect().height,
 
-  xScale() {
-    return d3.scaleLinear()
-      .domain([0, (d, i) => i])
-      .range([0, this.width])
-  },
-  yScale() {
-    return d3.scaleLinear()
-      .domain([0, d3.max(d => d[1])])
-      .range([0, this.height])
-  },
-})
+    xScale() {
+      return d3
+        .scaleLinear()
+        .domain([0, (d, i) => i])
+        .range([0, this.width]);
+    },
+    yScale() {
+      return d3
+        .scaleLinear()
+        .domain([0, d3.max(d => d[1])])
+        .range([0, this.height]);
+    }
+  });
 
-
-export default renderChart
+export default renderChart;
